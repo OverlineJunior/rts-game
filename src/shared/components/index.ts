57fb4f2ccdd76350129d440e9495568545fea5c7
over <franciscosebastianyjunior@hotmail.com>
2023-10-21 22:18:43 -0300

    Fix component Owner being named "Player"

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -6,5 +6,5 @@ export type Unit = ReturnType<typeof Unit>
 export const Position = component<{ value: Vector3 }>("Position")
 export type Position = ReturnType<typeof Position>
 
-export const Owner = component<{ player: Player }>("Player")
+export const Owner = component<{ player: Player }>("Owner")
 export type Owner = ReturnType<typeof Owner>

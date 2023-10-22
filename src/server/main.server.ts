81db9b91bd0f0991cdb6b698df75a1eab0ce07db
over <franciscosebastianyjunior@hotmail.com>
2023-10-22 01:46:06 -0300

    Implement system replicateUnitSpawn

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,11 +1,17 @@
 import { Players, ReplicatedStorage, ServerScriptService } from "@rbxts/services"
 import { Owner, Position, Unit } from "shared/components"
 import startMatter from "shared/startMatter"
+import { Replicated } from "./components"
 
 const containers = [ServerScriptService.server.systems, ReplicatedStorage.shared.systems]
 const serverState = {}
 const world = startMatter(containers, serverState)
 
 Players.PlayerAdded.Connect((player) => {
-	world.spawn(Unit({}), Owner({ player }), Position({ value: Vector3.zero }))
+	world.spawn(
+		Unit({}),
+		Owner({ player }),
+		Position({ value: Vector3.zero }),
+		Replicated({ finishedFor: new Array() }),
+	)
 })
---
+++ src/server/systems/replicateUnitSpawn.ts
@@ -0,0 +1,23 @@
+import { World } from "@rbxts/matter"
+import { Players } from "@rbxts/services"
+import { Replicated } from "server/components"
+import { spawnFor } from "server/entityReplication"
+import { Owner, Position, Unit } from "shared/components"
+
+function replicateUnitSpawn(world: World) {
+	for (const [id, repl, unit, owner, pos] of world.query(Replicated, Unit, Owner, Position)) {
+		const clientsLeft = Players.GetPlayers().filter((c) => !repl.finishedFor.includes(c))
+		if (clientsLeft.isEmpty()) continue
+
+		spawnFor(clientsLeft, [unit, owner, pos])
+
+		world.insert(
+			id,
+			repl.patch({
+				finishedFor: [...repl.finishedFor, ...clientsLeft],
+			}),
+		)
+	}
+}
+
+export = replicateUnitSpawn

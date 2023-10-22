09874c9eef130db2ee04ef827bbed6ccd14b0607
over <franciscosebastianyjunior@hotmail.com>
2023-10-21 21:48:00 -0300

    Define the entityReplication remotes

--- src/shared/remotes.ts
+++ src/shared/remotes.ts
@@ -1,5 +1,10 @@
+import { AnyComponent } from "@rbxts/matter"
 import Net from "@rbxts/net"
 
-const remotes = Net.Definitions.Create({})
+const remotes = Net.Definitions.Create({
+	spawnOnClient:
+		Net.Definitions.ServerToClientEvent<[serverId: number, serializedComponents: Map<string, AnyComponent>]>(),
+	despawnOnClient: Net.Definitions.ServerToClientEvent<[serverId: number]>(),
+})
 
 export = remotes

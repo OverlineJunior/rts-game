13da3493accb7e98b899e0e6bc579fa515aeef8e
over <franciscosebastianyjunior@hotmail.com>
2023-10-22 18:39:03 -0300

    Render units

---
+++ src/client/systems/renderUnit.ts
@@ -0,0 +1,21 @@
+import { World } from "@rbxts/matter"
+import { Workspace } from "@rbxts/services"
+import { Renderable } from "client/components"
+import { Unit } from "shared/components"
+import { makeUnitModel } from "shared/components/unit"
+
+function renderUnit(world: World) {
+	for (const [id, unit] of world.query(Unit).without(Renderable)) {
+		const model = makeUnitModel(unit.kind)
+		model.Parent = Workspace
+
+		world.insert(
+			id,
+			Renderable({
+				model,
+			}),
+		)
+	}
+}
+
+export = renderUnit

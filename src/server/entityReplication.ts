25f788fec8bec26e30fd2ea2b150bf399058fe2a
over <franciscosebastianyjunior@hotmail.com>
2023-10-22 02:21:06 -0300

    Function spawnFor's id is now generated externally

--- src/server/entityReplication.ts
+++ src/server/entityReplication.ts
@@ -5,26 +5,27 @@ import remotes from "shared/remotes"
 const spawnOnClient = remotes.Server.Get("spawnOnClient")
 const despawnOnClient = remotes.Server.Get("despawnOnClient")
 
-let serverId = 1
+const registeredIds: Array<number> = new Array()
 
 /**
  * Spawns a new entity in each of the clients' worlds with the given components.
  * Note that the components must be either shared or client-sided, otherwise they'll be ignored.
  * @param clients The clients which the entity will be spawned for.
+ * @param id The discriminator used for despawning the spawned entity later on.
  * @param components The components to spawn the entity with.
- * @return The new entity ID.
  */
-export function spawnFor(clients: Array<Player>, components: Array<AnyComponent>): number {
-	spawnOnClient.SendToPlayers(clients, serverId, serializeComponents(components))
-
-	return serverId++
+export function spawnFor(clients: Array<Player>, id: number, components: Array<AnyComponent>) {
+	spawnOnClient.SendToPlayers(clients, id, serializeComponents(components))
+	registeredIds.push(id)
 }
 
 /**
  * Removes the entity in each of the clients' worlds.
  * @param clients The clients which the entity will be despawned for.
- * @param id The entity's ID.
+ * @param id The entity's discriminator.
  */
 export function despawnFor(clients: Array<Player>, id: number) {
+	assert(registeredIds.includes(id), `Cannot despawn entity of ID "${id}" because it has never been spawned`)
+
 	despawnOnClient.SendToPlayers(clients, id)
 }

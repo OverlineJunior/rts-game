2ab103a5e5d34f00c7f3770810c8dbc9e3d7d70e
over <franciscosebastianyjunior@hotmail.com>
2023-10-22 19:56:56 -0300

    Fix GoalPositions.queue being an array of Position instead of Vector3

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -7,7 +7,7 @@ export type Position = ReturnType<typeof Position>
 export const Owner = component<{ player: Player }>("Owner")
 export type Owner = ReturnType<typeof Owner>
 
-export const GoalPositions = component<{ queue: Array<Position> }>("GoalPositions")
+export const GoalPositions = component<{ queue: Array<Vector3> }>("GoalPositions")
 export type GoalPositions = ReturnType<typeof GoalPositions>
 
 export { Unit }

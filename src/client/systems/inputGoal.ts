8ddb5113c75121f27ee4a02ee2b91ac43163b38d
over <franciscosebastianyjunior@hotmail.com>
2023-10-28 08:29:03 -0300

    Get goal from mouse input instead of a random value

--- src/client/systems/inputGoal.ts
+++ src/client/systems/inputGoal.ts
@@ -1,6 +1,7 @@
 import { World, useEvent } from "@rbxts/matter"
 import { Players, UserInputService } from "@rbxts/services"
 import { GoalPositions, Owner, Unit } from "shared/components"
+import { getMouseWorldPosition } from "shared/mouse"
 
 const CLIENT = Players.LocalPlayer
 const M1 = Enum.UserInputType.MouseButton1
@@ -10,14 +11,14 @@ function inputGoal(world: World) {
 	for (const [_, input, processed] of useEvent(UserInputService, "InputEnded")) {
 		if (input.UserInputType !== M1 || processed) continue
 
-		// ! Temporary. Get mouse's world position.
-		const goalPosition = new Vector3(math.random(-50, 50), 0, math.random(-50, 50))
+		const mousePos = getMouseWorldPosition(100)
+		const goalPos = new Vector3(mousePos.X, 0, mousePos.Z)
 		const clearGoals = !UserInputService.IsKeyDown(LSHIFT)
 
 		for (const [_, unit, owner] of world.query(Unit, Owner, GoalPositions)) {
 			if (owner.player !== CLIENT) continue
 
-			unit.remotes.sendGoalPosition.FireServer(goalPosition, clearGoals)
+			unit.remotes.sendGoalPosition.FireServer(goalPos, clearGoals)
 		}
 	}
 }

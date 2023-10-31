9de3a4720480f43d9edece913b2542d673d09bab
over <franciscosebastianyjunior@hotmail.com>
2023-10-31 01:18:00 -0300

    Refactor grondMovement and structure it to implement vertical position handling

--- src/shared/systems/groundMovement.ts
+++ src/shared/systems/groundMovement.ts
@@ -1,38 +1,46 @@
 import { World, useDeltaTime } from "@rbxts/matter"
 import { GoalPositions, Position, Speed } from "shared/components"
 
+// Returned position is clamped to goal, meaning when it reaches or "goes past" goal, it *is* goal.
+function getHorizontalPosition(currPos: Vector3, goal: Vector3, speed: number) {
+	const dt = useDeltaTime()
+	const dir = goal.sub(currPos).Unit
+	const dist = goal.sub(currPos).Magnitude
+	const reachedGoal = dist <= speed * dt
+
+	const newPos = reachedGoal ? goal : currPos.add(dir.mul(speed * dt))
+
+	return {
+		x: newPos.X,
+		z: newPos.Z,
+	}
+}
+
+function getVerticalPosition() {
+	return 0
+}
+
 function groundMovement(world: World) {
 	for (const [id, pos, goals, speed] of world.query(Position, GoalPositions, Speed)) {
 		if (goals.queue.isEmpty()) continue
 
-		const goalPos = goals.queue[0]
-		const dir = goalPos.sub(pos.value).Unit
-		const dist = goalPos.sub(pos.value).Magnitude
-		const dt = useDeltaTime()
-		const willFinishNow = dist <= speed.base * dt
+		const goal = goals.queue[0]
 
-		let newPos: Vector3
+		const xz = getHorizontalPosition(pos.value, goal, speed.base)
+		const y = getVerticalPosition()
+		const newPos = new Vector3(xz.x, y, xz.z)
 
-		if (willFinishNow) {
-			newPos = goalPos
+		const queue = goals.queue
 
-			const shifted = [...goals.queue]
-			shifted.shift()
-
-			world.insert(
-				id,
-				goals.patch({
-					queue: shifted,
-				}),
-			)
-		} else {
-			newPos = pos.value.add(dir.mul(speed.base * dt))
+		if (newPos.X === goal.X && newPos.Z === goal.Z) {
+			queue.shift()
 		}
 
 		world.insert(
 			id,
 			pos.patch({
 				value: newPos,
+				queue,
 			}),
 		)
 	}

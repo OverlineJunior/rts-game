2750bc69e2118378abacc2a1912d0820ac89f850
over <franciscosebastianyjunior@hotmail.com>
2023-12-21 18:26:13 -0300

    Fix replicateUnitDespawn working for entities with only Replicated instead of Replicated + Unit

--- src/server/systems/replicateUnitDespawn.ts
+++ src/server/systems/replicateUnitDespawn.ts
@@ -1,11 +1,12 @@
 import { World } from "@rbxts/matter"
 import { Replicated } from "server/components"
 import { despawnFor } from "server/entityReplication"
+import { Unit } from "shared/components"
 
 // Despawns the client version of the unit when it's no longer replicated.
 function replicateUnitDespawn(world: World) {
 	for (const [id, replRec] of world.queryChanged(Replicated)) {
-		if (!replRec.old || replRec.new) continue
+		if (!replRec.old || replRec.new || !world.get(id, Unit)) continue
 
 		despawnFor(replRec.old.finishedFor, id)
 	}

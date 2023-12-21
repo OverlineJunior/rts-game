078e243a939b36a35cbe51a5d7066b9ecde72874
over <franciscosebastianyjunior@hotmail.com>
2023-12-21 18:23:28 -0300

    Unit spawn replication should replicate components individually instead of only replicating units with all of them

--- src/server/systems/replicateUnitSpawn.ts
+++ src/server/systems/replicateUnitSpawn.ts
@@ -1,23 +1,24 @@
-import { World } from "@rbxts/matter"
+import { AnyEntity, World } from "@rbxts/matter"
 import { Players } from "@rbxts/services"
 import { Replicated } from "server/components"
 import { spawnFor } from "server/entityReplication"
 import { Goals, Owner, Position, Speed, Unit } from "shared/components"
 
+// If an unit has any of these components, they will be replicated to the clients.
+// This should be updated whenever units can have a new component that should replicated.
+const REPLICATED_COMPONENTS = [Owner, Position, Goals, Speed]
+
+function getRemainingComponents(world: World, id: AnyEntity) {
+	return REPLICATED_COMPONENTS.filter((ctor) => world.get(id, ctor) !== undefined).map((ctor) => world.get(id, ctor)!)
+}
+
 // Spawns the client version of the unit when Replicated is first added.
 function replicateUnitSpawn(world: World) {
-	for (const [id, repl, unit, owner, pos, goals, speed] of world.query(
-		Replicated,
-		Unit,
-		Owner,
-		Position,
-		Goals,
-		Speed,
-	)) {
+	for (const [id, repl, unit] of world.query(Replicated, Unit)) {
 		const clientsLeft = Players.GetPlayers().filter((c) => !repl.finishedFor.includes(c))
 		if (clientsLeft.isEmpty()) continue
 
-		spawnFor(clientsLeft, id, [unit, owner, pos, goals, speed])
+		spawnFor(clientsLeft, id, [unit, ...getRemainingComponents(world, id)])
 
 		world.insert(
 			id,

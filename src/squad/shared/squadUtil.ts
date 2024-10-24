d77b18e6662697f492a11351379b766c489eb458
over <franciscosebastianyjunior@hotmail.com>
2024-10-24 17:05:28 -0300

    Implement squadUtil.squadMemberCount

--- src/squad/shared/squadUtil.ts
+++ src/squad/shared/squadUtil.ts
@@ -1,5 +1,5 @@
 import { AnyEntity, World } from "@rbxts/matter"
-import { Position, Speed, Unit } from "game/shared/components"
+import { Position, Speed, SquadMember, Unit } from "game/shared/components"
 
 // This is a shared function, so don't forget that components required to be in a squad must be shared as well.
 export function canBeInSquad(id: AnyEntity, world: World): boolean {
@@ -7,4 +7,16 @@ export function canBeInSquad(id: AnyEntity, world: World): boolean {
 		&& world.get(id, Unit) !== undefined
 		&& world.get(id, Position) !== undefined
 		&& world.get(id, Speed) !== undefined
+}
+
+export function squadMemberCount(squadId: AnyEntity, world: World): number {
+	let count = 0
+
+	for (const [_, squadMember] of world.query(SquadMember)) {
+		if (squadMember.squad === squadId) {
+			count++
+		}
+	}
+
+	return count
 }

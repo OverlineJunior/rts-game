2ade025517722302f0555739a6f1f7438a2419e9
over <franciscosebastianyjunior@hotmail.com>
2024-10-25 13:43:15 -0300

    Remove outdated comments

--- src/unit/movement/server/replicateUnitPosition.ts
+++ src/unit/movement/server/replicateUnitPosition.ts
@@ -11,21 +11,12 @@ const REPLICATION_RATE = 1 / 10
 // ! but firing reliably when the unit *just started moving* or *just stopped moving*.
 // ! This idea comes from the fact that when something is ephemeral, its okay it being lost, but since
 // ! when the unit just started/stopped moving it will stay there, we must guarantee that the client matches that position.
-// We dont send in batches for performance gains because we want unit's positions to be independent from
-// each other. This way, a unit that dashes can dash without having to wait for the batch to be filled.
 function replicateUnitPosition(world: World) {
 	for (const [id, pos] of world.queryChanged(Position)) {
 		if (!pos.new || !world.contains(id) || !world.get(id, Unit)) continue
 		if (!useThrottle(REPLICATION_RATE, id)) continue
 
 		sendUnitPosition.FireAllClients(id, pos.new.value)
-
-		// Simulate extreme latency for testing purposes.
-		// if (math.random(0, 125) === 1) {
-		// 	task.delay(0.25, () => sendUnitPosition.FireAllClients(id, pos.new!.value))
-		// } else {
-		// 	sendUnitPosition.FireAllClients(id, pos.new.value)
-		// }
 	}
 }
 

cf783109260b37000635f06a500c624a9d71b668
over <franciscosebastianyjunior@hotmail.com>
2024-10-25 22:04:48 -0300

    Change unit prefab

--- src/unit/client/renderUnit.ts
+++ src/unit/client/renderUnit.ts
@@ -4,6 +4,8 @@ import { Renderable } from "game/client/components"
 import { System } from "game/shared/bootstrap"
 import { Unit } from "game/shared/components"
 
+const RNG = new Random()
+
 // TODO! Should clone the unit's prefab model based on its kind.
 function getTemporaryModel(): Model {
 	const model = new Instance("Model")
@@ -12,10 +14,12 @@ function getTemporaryModel(): Model {
 
 	const body = new Instance("Part")
 	body.Name = "body"
-	body.Size = Vector3.one
+	body.Size = new Vector3(0.75, RNG.NextNumber(0.95, 1), 0.75)
 	body.Anchored = true
 	body.BrickColor = BrickColor.random()
-	body.Material = Enum.Material.Neon
+	body.CanCollide = false
+	body.CanTouch = false
+	body.CanQuery = false
 	body.Parent = model
 
 	model.PrimaryPart = body

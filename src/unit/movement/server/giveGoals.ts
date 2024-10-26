68540cc5fecfe827a1d679890610a3c0944820d1
over <franciscosebastianyjunior@hotmail.com>
2024-10-25 22:04:56 -0300

    Add random goals

--- src/unit/movement/server/giveGoals.ts
+++ src/unit/movement/server/giveGoals.ts
@@ -6,6 +6,18 @@ import Queue from "game/shared/queue"
 import { requestMovement } from "game/shared/remotes"
 import { canRequestMovement } from "unit/shared/unitUtil"
 
+function randomizeGoal(goal: Vector3, unitCount: number): Vector3 {
+    const maxRadius = (unitCount - 1) * 0.075;
+
+    const angle = math.random() * 2 * math.pi;
+    const radius = math.random() * maxRadius;
+
+    const xOffset = radius * math.cos(angle);
+    const zOffset = radius * math.sin(angle);
+
+    return goal.add(new Vector3(xOffset, 0, zOffset));
+}
+
 function giveGoals(world: World) {
 	requestMovement.OnServerEvent.Connect((sender, serverUnits, goal, increment) => {
 		if (
@@ -18,10 +30,12 @@ function giveGoals(world: World) {
 		(serverUnits as AnyEntity[])
 			.filter(id => canRequestMovement(id, world) && world.get(id, Owner)?.player === sender)
 			.forEach(id => {
+				const rGoal = randomizeGoal(goal, serverUnits.size())
+
 				world.insert(id, Goal({
 					queue: increment && world.get(id, Goal)
-						? world.get(id, Goal)!.queue.enqueue(goal)
-						: new Queue(goal)
+						? world.get(id, Goal)!.queue.enqueue(rGoal)
+						: new Queue(rGoal)
 				}))
 			})
 	})

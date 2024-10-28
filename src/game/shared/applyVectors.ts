e9443feee8043e18ed4ab66b4ea8659dcaadf4c0
over <franciscosebastianyjunior@hotmail.com>
2024-10-28 18:29:53 -0300

    Fix applyVectors not applying delta time, resulting in overly fast movement

--- src/game/shared/applyVectors.ts
+++ src/game/shared/applyVectors.ts
@@ -1,12 +1,12 @@
-import { World } from "@rbxts/matter"
+import { useDeltaTime, World } from "@rbxts/matter"
 import { System } from "./bootstrap"
 import { Acceleration, Position, Velocity } from "./components"
 import { limit } from "./vector3"
 
 function applyVectors(world: World) {
 	for (const [id, pos, vel, acc] of world.query(Position, Velocity, Acceleration)) {
-		const newVel = limit(vel.value.add(acc.value), 0.1)
-		const newPos = pos.value.add(newVel)
+		const newVel = vel.value.add(acc.value)
+		const newPos = pos.value.add(newVel.mul(useDeltaTime()))
 
 		world.insert(
 			id,

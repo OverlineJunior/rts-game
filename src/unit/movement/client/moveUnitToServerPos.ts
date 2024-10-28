f61522273f50177bae7b58425640b228f580369e
over <franciscosebastianyjunior@hotmail.com>
2024-10-28 18:14:53 -0300

    Make moveUnitToServerPos temporarely snap positions since previous implementation was wrong

--- src/unit/movement/client/moveUnitToServerPos.ts
+++ src/unit/movement/client/moveUnitToServerPos.ts
@@ -1,23 +1,12 @@
-import { World, useDeltaTime } from "@rbxts/matter"
+import { World } from "@rbxts/matter"
 import { ServerPosition } from "game/client/components"
 import { System } from "game/shared/bootstrap"
 import { Position, Speed } from "game/shared/components"
 
-function getNextPosition(pos: Vector3, goal: Vector3, speed: number): Vector3 {
-	const dt = useDeltaTime()
-	const dir = goal.sub(pos).Unit
-	const dist = goal.sub(pos).Magnitude
-	const reachesGoal = dist <= speed * dt
-	const offset = dir.mul(speed * dt)
-
-	return reachesGoal ? goal : pos.add(offset)
-}
-
 function moveUnitToServerPos(world: World) {
-	for (const [id, pos, serverPos, speed] of world.query(Position, ServerPosition, Speed)) {
-		world.insert(id, pos.patch({
-			value: getNextPosition(pos.value, serverPos.value, speed.value)
-		}))
+	for (const [id, pos, serverPos] of world.query(Position, ServerPosition, Speed)) {
+		// TODO! Implement a proper interpolation system for smooth movements.
+		world.insert(id, pos.patch({ value: serverPos.value }))
 	}
 }
 

3cf6ecb917e328628845ebb52601865013c59447
over <franciscosebastianyjunior@hotmail.com>
2025-04-09 22:24:55 -0300

    Add a heightmap to the server state and remove old heightmap tests in main.server

--- src/game/server/main.server.ts
+++ src/game/server/main.server.ts
@@ -7,19 +7,8 @@ import Heightmap from "game/shared/heightmap"
 const systems = [ServerScriptService.server, ReplicatedStorage.shared]
 
 const state: ServerState = {
-	unitGrid: new SpatialGrid(4)
+	unitGrid: new SpatialGrid(4),
+	heightmap: new Heightmap(Workspace.FindFirstChild("map")! as Model),
 }
 
 const world = runGame(systems, state)
-
-const map = new Heightmap(Workspace.FindFirstChild("map")! as Model)
-const part = Workspace.FindFirstChild("Part")! as Part
-
-while (true) {
-	task.wait()
-
-	const cell = map.get(part.Position)
-	if (!cell) continue
-
-	print(`${cell.altitude}, ${cell.part}`)
-}
--- src/game/server/serverState.ts
+++ src/game/server/serverState.ts
@@ -1,6 +1,8 @@
 import { AnyEntity } from "@rbxts/matter"
+import Heightmap from "game/shared/heightmap"
 import { SpatialGrid } from "game/shared/spatialGrid"
 
 export interface ServerState {
 	unitGrid: SpatialGrid<AnyEntity>
+	heightmap: Heightmap
 }
